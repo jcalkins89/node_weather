b6ce15e3de4782e62585fdbaae3a1255ab003a86
Josh Calkins <jcalkins89@gmail.com>
2019-01-24 13:48:12 -0500

    Abstract callback function to separate file for improved readability and organization

--- app.js
+++ app.js
@@ -1,35 +1,24 @@
-const request = require('request');
 const yargs = require('yargs');
 
+const geocode = require('./geocode/geocode.js');
+
 const argv = yargs
-    .options({
-        address: {
-            demand: true,
-            alias: 'a',
-            describe: 'Address to fetch weather for',
-            string: true
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
-
-    console.log(argv);
-    var encodedAddress = encodeURIComponent(argv.a);
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=<PUT YOUR API KEY HERE>`,
-    json: true
-}, (error, response, body) => {
-    if(error){
-        console.log('Unable to connect to Google servers.');
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('Unable to find that address.');
-    } else if (body.status === 'OVER_QUERY_LIMIT'){
-        console.log('You have reached the maximum query limit for this API. Sign up for a developers console');
-    } else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
     }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
+  }
 });
